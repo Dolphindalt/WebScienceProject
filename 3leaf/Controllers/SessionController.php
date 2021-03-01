eeac0f60e631c8cb3ee2d38a6e781765e197d308
Dalton C <dcaron@mtech.edu>
2021-03-01 16:30:19 -0700

    Update username regex.

--- 3leaf/Controllers/SessionController.php
+++ 3leaf/Controllers/SessionController.php
@@ -60,7 +60,7 @@ class Session extends ControllerBase {
             die();
         }
 
-        if (!preg_match('/[a-zA-Z0-9]+/', $username)) {
+        if (!preg_match('/^[a-zA-Z0-9]{3,16}$/', $username)) {
             http_response_code(409);
             $this->error = "Usernames should only contain alphanumeric characters.";
             $this->showRegisterPageTask();
